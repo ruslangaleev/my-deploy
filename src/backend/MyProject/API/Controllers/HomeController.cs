f7115b6f6516fb84d5201071e46b165f9ccbd057
Ruslan Galeev <ruslan.nafisovich@gmail.com>
2021-04-07 20:26:28 +0500

    Инициализация проекта

---
+++ src/backend/MyProject/API/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var message = "Welcome to my service";
+
+            return Ok(message);
+        }
+    }
+}
